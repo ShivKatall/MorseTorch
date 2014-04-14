c52690ff964fc6bce538491370cebffdb21022ce
Cole Bratcher <colebratcher@gmail.com>
2014-04-14 13:30:51 -0700

    Initial Commit

---
+++ MorseTorch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MorseTorch
+//
+//  Created by Cole Bratcher on 4/14/14.
+//  Copyright (c) 2014 Cole Bratcher. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
